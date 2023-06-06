04be14e48e7be622206fef19bf722a975519b263
Cyberfox206 <bbrontesi@edenschool.fr>
2023-06-06 16:27:33 +0200

    complite project

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const b1 = document.getElementById("fs");
+const b2 = document.getElementById("fs1");
+const b3 = document.getElementById("fs2");
+const b4 = document.getElementById("fs3");
+const b5 = document.getElementById("fs4");
+
+function random(element) {
+    if (element === b1.id) {
+        b2.style.
+    }
+}
+
+b1.addEventListener("click", () => { random(b1) })
